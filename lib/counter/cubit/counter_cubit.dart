7840ea941123016713e0993e6c7aeda8939e0aac
Lucas Alves <l.alves001@gmail.com>
2022-02-05 23:26:05 -0300

    Added forgotten emit at cubit

--- lib/counter/cubit/counter_cubit.dart
+++ lib/counter/cubit/counter_cubit.dart
@@ -3,7 +3,7 @@ import 'package:bloc/bloc.dart';
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
 
-  void increment() => (state + 1);
+  void increment() => emit(state + 1);
 
-  void decrement() => (state - 1); 
+  void decrement() => emit(state - 1);
 }
